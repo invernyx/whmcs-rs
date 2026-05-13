7b2d76cae58d465d127e3cfb793e8663302b046f
Fabio Almeida <me@fabioalmeida.dev>
2026-05-13 23:11:20 +0100

    fix: :bug: correct test client behaviour

--- src/test.rs
+++ src/test.rs
@@ -4,8 +4,8 @@ pub fn get_test_client() -> WhmcsClient {
     dotenvy::dotenv().ok();
 
     let url = std::env::var("WHMCS_URL").expect("WHMCS_URL must be set");
-    let id = std::env::var("WHMCS_IDENTIFIER").expect("WHMCS_IDENTIFIER must be set");
-    let secret = std::env::var("WHMCS_SECRET").expect("WHMCS_SECRET must be set");
+    let id = std::env::var("WHMCS_API_IDENTIFIER").expect("WHMCS_API_IDENTIFIER must be set");
+    let secret = std::env::var("WHMCS_API_SECRET").expect("WHMCS_API_SECRET must be set");
 
     WhmcsBuilder::new()
         .url(url)
